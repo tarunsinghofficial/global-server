f33746c7d546de2eef359de13d6ec8b57ff88d50
Adeebahmed333 <adeebahmed3337@gmail.com>
2024-04-14 19:11:05 +0530

    Fixed Bug In lifestyle-request-time-verify middleware

--- src/middlewares/lifestyle-request-time-verify.js
+++ src/middlewares/lifestyle-request-time-verify.js
@@ -3,22 +3,23 @@ import Lifestyle from '../models/lifestyle-model.js';
 export const checkLifestyleEntry = async (req, res, next) => {
     const userId = req.body.userId;
 
-    const currentMonth = new Date().getMonth() + 1;
-    const currentYear = new Date().getFullYear();
-    
     try {
-        const existingEntry = await Lifestyle.findOne({
-            userId: userId,
-            createdAt: { $gte: new Date(currentYear, currentMonth - 1, 1), $lt: new Date(currentYear, currentMonth, 1) }
+        const lifestyle = await Lifestyle.findOne({ 
+            userId:userId 
         });
-
-        if (existingEntry) {
-            return res.status(400).json({ 
-                success:false,
-                data:{},
-                message:"Something Went Wrong",
-                error: 'A lifestyle entry already exists for this month.'
-             });
+        if(lifestyle) {
+            const existingEntry = await Lifestyle.findOne({
+                userId: userId,
+                createdAt: { $gte: new Date(lifestyle.createdAt), $lt: new Date(lifestyle.createdAt.getTime() + (30 * 24 * 60 * 60 * 1000)) }
+            });
+            if (existingEntry) {
+                return res.status(400).json({ 
+                    success:false,
+                    data:{},
+                    message:"Something Went Wrong",
+                    error: 'A lifestyle entry already exists for this month.'
+                 });
+            }
         }
 
         next();
